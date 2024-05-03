0b82bc4e70f1a49cd4afe7bbe24e204e2d332791
msrody <42296713+msrody@users.noreply.github.com>
2024-05-03 12:16:41 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -37,7 +37,7 @@ export default defineAppConfig({
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://www.linkedin.com/in/michael-rody/'
       },
 
       github: 'msrody',
