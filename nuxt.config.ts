2dcf0847e9e41826bb70bb893102cfec66ecf4da
msrody <mike.rody@gmail.com>
2024-07-26 15:12:46 -0700

    update dist

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
-
+    "nitro": {
+        "output": {
+            dir: 'dist',
+            serverDir: 'dist/server',
+            publicDir: 'dist/public'
+        }
+    },
   modules: [
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
     // https://github.com/nuxt/devtools
     '@nuxt/devtools',
-    '@nuxthq/studio'
-
-  ],
-
-  compatibilityDate: '2024-07-26'
+  ]
 })
