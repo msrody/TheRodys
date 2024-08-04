ef74a54a94f63373b1fd63e50276d625c447c307
msrody <mike.rody@gmail.com>
2024-08-03 21:10:00 -0700

    e

--- components/content/GalleryPortrait.vue
+++ components/content/GalleryPortrait.vue
@@ -31,14 +31,14 @@ css({
     '.layout': {
       display: 'grid',
       gap: '{space.8}',
-      my: '{space.6}',
+      my: '{space.16}',
       '--cols': 1,
       gridTemplateColumns: 'repeat(var(--cols), minmax(0, 1fr))',
       '@md': {
         '--cols': (props) => props.images.length < 2 ? props.images.length : 2
       },
       img: {
-        objectFit: 'contain',
+        objectFit: 'cover',
         height: '100%',
         aspectRatio: '16 / 9',
         borderRadius: '{radii.md}'
