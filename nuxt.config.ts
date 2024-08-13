7b6a8752abdd17caabd22f6f8e5c311168a1a789
msrody <42296713+msrody@users.noreply.github.com>
2024-08-12 17:46:22 -0700

    nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
 
+  "nitro": {
+        "dist": {
+            dir: 'dist',
+            serverDir: 'dist/server',
+            publicDir: 'dist/public'
+        }
+    },
+
   modules: [
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible',
@@ -9,5 +17,4 @@ export default defineNuxtConfig({
     '@nuxt/devtools',
   ],
 
-  compatibilityDate: '2024-08-03'
 })
