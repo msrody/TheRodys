594585c10fa51a7a125af3a9b234dd32a8185ad5
msrody <42296713+msrody@users.noreply.github.com>
2025-04-03 17:20:39 -0700

    Social Links Header setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
+  app: {
+    head: {
+      title: 'My Nuxt 3 Site',
+      meta: [
+        { hid: 'description', name: 'description', content: "The Rody's Missions page" },
+        { hid: 'og:title', property: 'og:title', content: 'The Rodys' },
+        { hid: 'og:description', property: 'og:description', content: "The Rody's Missions page" },
+        { hid: 'og:url', property: 'og:url', content: 'https://therodys.com' },
+        { hid: 'og:image', property: 'og:image', content: 'https://dev----wherewevebeen.therodys.pages.dev/FinalTouch169DSC09811_Croped16x9_smallest.jpg' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+      ],
+    },
+  },
 
   "nitro": {
         "dist": {
